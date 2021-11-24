fd7efd06e578f2eed43881b60ab1ba30c025e653
Divyanshu Pachisia <divyanshupachisia@princeton.edu>
2021-11-24 14:42:00 -0500

    defined functions for utils_prob_env

---
+++ gym_reachability/gym_reachability/prob_envs/utils_prob_env.py
@@ -0,0 +1,18 @@
+
+'''
+Generate a 10 by 10 grid with values between 0 and 1 at each index
+Values represent probability of obstacle 
+'''
+def gen_grid(): 
+
+    
+    return grid
+
+'''
+Given a sensing radius, R, the current robot position, cur_pos, and a grid environment 
+return the subset of the grid environment as a vector that falls within the sensing radius
+'''
+def get_local_map(R, grid, cur_pos):
+
+    
+    return local_map  
